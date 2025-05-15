056ffdc006137a4474ca7d6b18944c5d09e94e02
Gokul653 <gokulr23csa@vetias.ac.in>
2025-05-15 11:47:27 +0530

    multiplication table.c

---
+++ multiplication table.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    
+    scanf("%d %d", &a,&b);
+    int i=1;
+
+ while(i<=50)
+ {
+  
+    printf("%d * %d = %d\n ", i,a,i*a);
+
+i++;
+}
+return 0;
+}
+
